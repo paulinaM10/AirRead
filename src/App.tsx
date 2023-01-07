8e6ffdc8b1df7ad52278004d4b22c5a28bf91735
Paulina Malek <paulina302aa@gmail.com>
2023-01-07 11:36:37 +0000

    pam

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {ExploreTopBooks} from "./layouts/HomePage/ExploreTopBooks";
+import {Carousel} from "./layouts/HomePage/Carousel";
+import {Heros} from "./layouts/HomePage/Heros";
+import {LibraryServices} from "./layouts/HomePage/LibraryServices";
 
 function App() {
   return (
-    <div>
-    </div>
+      <div>
+          <Navbar />
+          <ExploreTopBooks />
+          <Carousel/>
+          <Heros />
+          <LibraryServices />
+
+
+      </div>
   );
 }
 
