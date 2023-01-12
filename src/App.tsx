e462df0f8c8d58b14d3ce2b840767342bd62ca46
Paulina Malek <paulina302aa@gmail.com>
2023-01-12 19:19:36 +0000

    pam

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {HomePage} from "./layouts/HomePage/HomePage";
+import {SearchBookPage} from "./layouts/SearchBooksPage/SearchBookPage";
 
 
 export const App = () => {
   return (
       <div>
           <Navbar />
-          <HomePage />
+          {/* <HomePage /> */}
+          <SearchBookPage />
           <Footer />
 
       </div>
--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
+import BookModel from "../../../models/BookModel";
 
 
-export const ReturnBook = () => {
-    return (
+// get book img but if not available use generic img, after that title, author
+
+export const ReturnBook: React.FC<{book: BookModel}> = (props) => {
+  return (
         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
             <div className='text-center'>
-                <img src={require('../../../Images/BooksImages/marcus.jpeg')}
-                     width='151'
-                     height='233'
-                     alt="book"
-                />
-                <h6 className='mt-2'>Book</h6>
-                <p>AR</p>
+
+                {props.book.img ?
+                    <img
+                        src={props.book.img}
+                        width='151'
+                        height='233'
+                        alt="book"
+                    />
+                    :
+                    <img
+                        src={require('../../../Images/BooksImages/marcus.jpeg')}
+                        width='151'
+                        height='233'
+                        alt="book"
+                    />
+                }
+
+                <h6 className='mt-2'>{props.book.title}</h6>
+                <p>{props.book.author}</p>
                 <a className='btn btn-outline-secondary text-black' href='src/layouts/HomePage/components/ReturnBook#'>Reserve</a>
             </div>
         </div>
 
     );
 }
+
