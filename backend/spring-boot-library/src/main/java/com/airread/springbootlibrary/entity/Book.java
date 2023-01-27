723bf3a9a72cce6938690c4eeacb639d5e43e42f
paulinaM10 <paulina302aa@gmail.com>
2023-01-27 12:11:31 +0000

    Add files via upload

---
+++ backend/spring-boot-library/src/main/java/com/airread/springbootlibrary/entity/Book.java
@@ -0,0 +1,39 @@
+package com.airread.springbootlibrary.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+//importing lombok, jakarta. getting data in tables from sql
+
+@Entity
+@Table(name= "book")
+@Data
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copiesAvailable")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+}
